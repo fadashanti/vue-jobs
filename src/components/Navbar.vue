090b171d6d5f4e2c2a070e11b2a3a0485304676d
fadashanti <fadashanti09@gmail.com>
2025-02-14 13:21:58 +0100

    Update Navbar color scheme from green to blue

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,7 +3,7 @@ import logo from "/vue.svg";
 </script>
 
 <template>
-  <nav class="bg-green-700 border-b border-green-500">
+  <nav class="bg-blue-700 border-b border-blue-500">
     <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
       <div class="flex h-20 items-center justify-between">
         <div
